bdfe04055a50d03d687bccffd9883a82f13251ef
vagrant <vagrant@vagrant.vm>
2018-11-05 10:02:04 +0000

    add to_print function

--- main.py
+++ main.py
@@ -24,22 +24,14 @@ def get_most_populer_articles():
     where articles.slug = stats.article_name group by articles.title order by sum DESC;'''
     cursor = db.cursor()
     cursor.execute(query)
-
-    print('------------------------------')
-    print('The Most Populer Articles of All Time \n')
-    for record in cursor:
-        print('{} --- {} Views'.format(record[0], record[1]))
+    return cursor
 
 
 def get_most_populer_authors():
     query = '''select authors.name, top_authors.sum from authors, (select articles.author, sum(newlog.hits) from articles, (select substr(log.path,10,50) as article_name, count(*) as hits from log where path like '/article/%' group by path order by hits DESC) as newlog where articles.slug = newlog.article_name group by articles.author order by sum DESC) AS top_authors where authors.id = top_authors.author;'''
-
     cursor = db.cursor()
     cursor.execute(query)
-    print('------------------------------')
-    print('The Most Populer Authors of All Time \n')
-    for record in cursor:
-        print('{} --- {} Views'.format(record[0], record[1]))
+    return cursor
 
 
 def get_most_errors():
@@ -47,17 +39,24 @@ def get_most_errors():
     '''
     cursor = db.cursor()
     cursor.execute(query)
-    print('------------------------------')
-    print('Days were errors > 1% \n')
-    for record in cursor:
-        print('{} --- {} Errors'.format(record[0], record[1]))
+    return cursor
 
+
+def to_print(curser, intro, suffex):
+    print('----------------------------')
+    print('{} \n'.format(intro))
+    for record in curser:
+        print('{} --- {} {}'.format(str(record[0]).ljust(35),str(record[1]).ljust(10), suffex))
 
 
 def main():
-    get_most_populer_articles()
-    get_most_populer_authors()
-    get_most_errors()
+    intro = {'articles': 'The Most Populer Articles of All Time',
+             'authors': 'The Most Populer Authers of all time', 'errors': 'Days were error is > 1%'}
 
+    to_print(get_most_populer_articles(), intro['articles'], suffex=' Views' )
+
+    to_print(get_most_populer_authors(), intro['authors'], ' Views')
+
+    to_print(get_most_errors(), intro['errors'], '% Errors')
 
 main()
